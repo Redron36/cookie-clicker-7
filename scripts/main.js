345310e5d38a5b044c43bb9e831b442ff1d0ead7
Az989YT <az989yt@gmail.com>
2021-11-18 17:11:44 -0500

    yay

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+
+let helpers = [
+    'Cursor',
+    'Grandma',
+    'Farm',
+    'Mine',
+    'Factory',
+    'Bank',
+    'Temple'
+];
+
+if (helpers[0]) {
+    alert('Helper is a cursor.');
+}

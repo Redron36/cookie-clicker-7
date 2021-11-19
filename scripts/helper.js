359d1a5f68fdba9ab9d5f4ad76fbbe4ae5a6cce0
Az989YT <az989yt@gmail.com>
2021-11-18 20:25:40 -0500

    added in helpers baking

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,6 +1,9 @@
 var clickerPrice = 10;
 var grandmaPrice = 100;
 
+var clickerPower = 0.1;
+var grandmaPower = 1;
+
 function buyClicker(){ // click button make click stronger
     if(cookies >= clickerPrice)
     {
@@ -20,3 +23,19 @@ function buyGrandma(){ // click button make click stronger
         grandmas.innerHTML = grandma;
     }
 }
+
+window.setInterval(function clickerBake(){
+    var clickerCPS = 1;
+
+    cookies = cookies + (clickerCPS * clicker);
+    counter.innerHTML = cookies;
+
+}, 1000);
+
+window.setInterval(function grandmaBake(){
+    var grandmaCPS = 5;
+
+    cookies = cookies + (grandmaCPS * grandma);
+    counter.innerHTML = cookies;
+
+}, 1000);

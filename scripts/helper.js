dc4b1ba3e65e7e1e9bf2ebba23b765b415634a6f
Az989YT <az989yt@gmail.com>
2021-11-19 16:13:21 -0500

    animation and other stuff

--- scripts/helper.js
+++ scripts/helper.js
@@ -6,13 +6,22 @@ var clickerPrice = 10;
 var grandmaPrice = 100;
 var farmPrice = 250;
 
+var clickerCPS = 1;
+var grandmaCPS = 5;
+var farmCPS = 5;
+
+var costMult = 1;
+
 function buyClicker(){ // click button make click stronger
     if(cookies >= clickerPrice)
     {
         clicker = clicker + 1;
         cookies = cookies - clickerPrice;
+        clickerPrice =  Math.round(clickerPrice * (power + clicker*costMult/2));
         counter.innerHTML = cookies;
         clickers.innerHTML = clicker;
+        clickerCost.innerHTML = clickerPrice;
+        costMult = Math.round(costMult*costMult/2);
     }
 }
 
@@ -21,8 +30,11 @@ function buyGrandma(){ // click button make click stronger
     {
         grandma = grandma + 1;
         cookies = cookies - grandmaPrice;
+        grandmaPrice =  Math.round(grandmaPrice * (power + grandma*costMult/2));
         counter.innerHTML = cookies;
         grandmas.innerHTML = grandma;
+        grandmaCost.innerHTML = grandmaPrice;
+        costMult = Math.round(costMult * costMult);
     }
 }
 
@@ -31,30 +43,27 @@ function buyFarm(){ // click button make click stronger
     {
         farm = farm + 1;
         cookies = cookies - farmPrice;
+        farmPrice =  Math.round(farmPrice * (power + farm*costMult));
         counter.innerHTML = cookies;
         farms.innerHTML = farm;
+        farmCost.innerHTML = farmPrice;
+        costMult = Math.round(costMult*costMult/2);
     }
 }
 
 window.setInterval(function clickerBake(){
-    var clickerCPS = 1;
-
     cookies = cookies + (clickerCPS * clicker);
     counter.innerHTML = cookies;
 
 }, 1000);
 
 window.setInterval(function grandmaBake(){
-    var grandmaCPS = 5;
-
     cookies = cookies + (grandmaCPS * grandma);
     counter.innerHTML = cookies;
 
 }, 1000);
 
 window.setInterval(function farmBake(){
-    var farmCPS = 10;
-
     cookies = cookies + (farmCPS * farm);
     counter.innerHTML = cookies;
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
 // basic cookie stuff
 var cookies = 0;
 var power = 1;
+var cps = 0;
 
 function increment() { // click cookie make cookie
+    const element = document.getElementById('cookie')
+
+    element.classList.remove('animation-target'); // reset animation
+    void element.offsetWidth; // trigger reflow
+    element.classList.add('animation-target'); // start animation
+
     cookies = cookies + power;
     counter.innerHTML = cookies;
 }
+
+window.setInterval(function updateCPS(){
+    cps = (clicker * clickerCPS) + (grandma * grandmaCPS) + (farm * farmCPS);
+    cpsCount.innerHTML = cps;
+}, 1000);
+
+document.getElementById('cookie').ondragstart = function() { return false; };
+document.getElementsByClassName('helpers').ondragstart = function() { return false; };

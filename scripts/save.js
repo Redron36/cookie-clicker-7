3f14235c0519a59de9da1f2eb7bcbc0f5b65f000
Az989YT <az989yt@gmail.com>
2021-11-30 22:00:45 -0500

    SAVING HECK YEAH LETS GOOOOOOOOOOOO

--- scripts/save.js
+++ scripts/save.js
@@ -1,1 +1,20 @@
-document.cookie = "totalBaked = " + cookies.toString;
+function save() {
+    localStorage.setItem('cookies', JSON.stringify(cookies));
+    localStorage.setItem('clickers', JSON.stringify(clicker));
+    localStorage.setItem('grandmas', JSON.stringify(grandma));
+    localStorage.setItem('farms', JSON.stringify(farm));
+    localStorage.setItem('factories', JSON.stringify(factory));
+  }
+  
+  function load() {
+    cookies = JSON.parse(localStorage.getItem('cookies'));
+    clicker = JSON.parse(localStorage.getItem('clickers'));
+    grandma = JSON.parse(localStorage.getItem('grandmas'));
+    farm = JSON.parse(localStorage.getItem('farms'));
+    factory = JSON.parse(localStorage.getItem('factories'));
+    window.alert('Data Loaded.');
+  }
+
+  window.setInterval(function autosave(){
+    save();
+}, 300000);
